09657ff990ec8dfc7f1a484769c846915c598056
CarlosCastroFernandez <bubachico@gmail.com>
2025-12-05 17:35:41 +0100

    Pyecto en prueba

--- PA_FRONTEND/src/codeJS/apiFetch.js
+++ PA_FRONTEND/src/codeJS/apiFetch.js
@@ -1,120 +1,167 @@
 export async function getAllUser() {
-    try {
+  try {
+    const response = await fetch("http://localhost:3000/getAllUser", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": localStorage.getItem("token"),
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+    if (data.status === "Failed") return null;
+    const users = data.data;
+    console.log(data.data);
 
-        const response = await fetch("http://localhost:3000/getAllUser", {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "auth-token": localStorage.getItem("token")
-            }
-        })
-        const data = await response.json();
-        console.log(data);
-        if (data.status === "Failed") return null;
-        const users = data.data;
-        console.log(data.data);
-       
-        return users
-
-
-    } catch (error) {
-
-    }
+    return users;
+  } catch (error) {}
 }
 
 export async function sendLogin(email, password) {
-    let data;
-    try {
-        const response = await fetch("http://localhost:3000/getAllUser/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                email: email,
-                password: password
-            })
-        })
+  let data;
+  try {
+    const response = await fetch("http://localhost:3000/getAllUser/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
 
-         data = await response.json()
-        if (data.status === "Failed") return new Error("Error en la respuesta");
-        localStorage.setItem("token", data.token)
-        return data;
-
-    } catch (error) {
-        return data={
-            status:"Failed",
-            meesage: error.message
-        }
-    }
-
+    data = await response.json();
+    if (data.status === "Failed") return new Error("Error en la respuesta");
+    localStorage.setItem("token", data.token);
+    localStorage.setItem("token-refresh", data.tokenRefresh);
+    return data;
+  } catch (error) {
+    return (data = {
+      status: "Failed",
+      meesage: error.message,
+    });
+  }
 }
 
 export async function editUser(user) {
-    let data;
-    try {
+  let data;
+  try {
+    const response = await fetch(
+      "http://localhost:3000/getAllUser/edit?identification=" + user._id,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": localStorage.getItem("token"),
+        },
+        body: JSON.stringify(user),
+      }
+    );
+    data = await response.json();
+    console.log(data);
 
-        const response = await fetch("http://localhost:3000/getAllUser/edit?identification=" + user._id, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-                "auth-token": localStorage.getItem("token")
-            },
-            body: JSON.stringify(user)
-        })
-        data = await response.json();
-        console.log(data);
+    if (!response.ok) return null;
 
-        if (!response.ok) return null
-
-        return data.data
-    } catch (error) {
-        return data = error.message
-    }
+    return data.data;
+  } catch (error) {
+    return (data = error.message);
+  }
 }
 export async function deleteUser(id) {
-    let data;
-    try {
+  let data;
+  try {
+    const response = await fetch(
+      "http://localhost:3000/getAllUser/delete?id=" + id,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token": localStorage.getItem("token"),
+        },
+      }
+    );
+    data = await response.json();
+    console.log(data);
 
-        const response = await fetch("http://localhost:3000/getAllUser/delete?id=" + id, {
-            method: "DELETE",
-            headers: {
-                "Content-Type": "application/json",
-                "auth-token": localStorage.getItem("token")
-            }
-        })
-        data = await response.json();
-        console.log(data);
+    if (!response.ok) return null;
+    console.log(data.status);
 
-        if (!response.ok) return null
-        console.log(data.status);
-
-        return data.status
-    } catch (error) {
-        return data = error.message
-    }
+    return data.status;
+  } catch (error) {
+    return (data = error.message);
+  }
 }
 
-
 export async function createNewUser(user) {
-    let data;
+  let data;
+  try {
+    const response = await fetch("http://localhost:3000/getAllUser/newUser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": localStorage.getItem("token"),
+      },
+      body: JSON.stringify(user),
+    });
+    data = await response.json();
+    console.log(data);
+
+    if (!response.ok) return null;
+
+    return data.data;
+  } catch (error) {
+    return (data = error.message);
+  }
+}
+
+export async function getNewToken() {
+    console.log("NEW TOKEN GIVE");
+    
+  try {
+    const tokenActual = localStorage.getItem("token");
+    const response = await fetch("http://localhost:3000/getAllUser/newToken", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token": tokenActual,
+      },
+    });
+    if (!response.ok) throw new Error("Ha ocurrido un error");
+    const data = await response.json();
+    const token = data.tokenNew;
+    localStorage.setItem("token", token);
+    console.log("EL PRIMER TOKEN ES VALIDO");
+    
+    return true;
+  } catch (error) {
+    console.log("EL PRIMER TOKEN NO ES VALIDO");
+    
     try {
-
-        const response = await fetch("http://localhost:3000/getAllUser/newUser", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "auth-token": localStorage.getItem("token")
-            },
-            body: JSON.stringify(user)
-        })
-        data = await response.json();
-        console.log(data);
-
-        if (!response.ok) return null
-
-        return data.data
+      console.log(error.message);
+      const tokenRefresh = localStorage.getItem("token-refresh");
+      const response = await fetch(
+        "http://localhost:3000/getAllUser/newToken",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "auth-token": tokenRefresh,
+          },
+        }
+      );
+      if (!response.ok) throw new Error("Ha ocurrido un error");
+      const data = await response.json();
+      const token = data.tokenNew;
+      console.log(token);
+      localStorage.setItem("token", token);
+      console.log("SEGINDO TOKEN ES VALIDO");
+      
+      return true;
     } catch (error) {
-        return data = error.message
+        console.log("SEGUNDO TOKEN NO ES VALIDO "+error.message);
+        
+        return false
     }
+  }
 }
--- PA_FRONTEND/src/main.js
+++ PA_FRONTEND/src/main.js
@@ -1,13 +1,26 @@
 import "./style.scss";
 import { login } from "./codeJS/login";
+import { getNewToken } from "./codeJS/apiFetch";
+import { createHeader } from "./codeJS/headerTable";
+import { createMain } from "./codeJS/table";
 
 
+if (localStorage.getItem("token")===null||localStorage.getItem("token")==="undefined"){
+  await login();
+}else{
+  if(await getNewToken()){
+    sessionStorage.clear()
+    createHeader();
+    await createMain();
+    document.querySelector(".section-login").style.display="none"
+  }else{
+    console.log("NADA VALIDO AMIGOS BVUELTA A LOGIN");
+    localStorage.clear();
+    sessionStorage.clear();
+    await login();
+  }
+}
 
-
-
-
-
-await login();
 
 
 document.addEventListener("click",(e)=>{
